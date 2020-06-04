a5fe5fb46b18a83ef01148a4739ff3a903bec30a
MartijnVanZelst <mpmj.vanzelst@student.avans.nl>
2020-06-04 17:39:08 +0200

    Changed Board

--- Netwerk/src/Board/Board.java
+++ Netwerk/src/Board/Board.java
@@ -1,11 +1,40 @@
 package Board;
 
 import Board.Tile.Tile;
+import Pieces.Piece;
+
+import java.util.Map;
 
 public class Board {
 
+    private Board(Builder builder){
+
+    }
 
     public Tile getTile(int tileCoordinate) {
         return null;
+    }
+
+    public static class Builder {
+
+        private Map<Integer, Piece> boardConfig;
+        private Piece piece;
+
+        public Builder() {
+
+        }
+
+        public Builder setPiece (Piece piece) {
+            this.boardConfig.put(piece.getPiecePosition(), piece);
+            return this;
+        }
+
+        public Builder setMoveMaker (Piece piece) {
+            boolean this.piece.isWhite() =  piece.isWhite();
+        }
+
+        public Board builder() {
+            return new Board(this);
+        }
     }
 }
