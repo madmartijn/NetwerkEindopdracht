e7e1c337352d2506bb41d214ffa462749b0c070f
Bipin <floriantjager@gmail.com>
2020-06-28 16:46:35 +0200

    Pawn move limit added

--- Netwerk/src/Pieces/Pawn.java
+++ Netwerk/src/Pieces/Pawn.java
@@ -11,10 +11,12 @@ public class Pawn extends Piece{
 
     private static int[] CANDIDATE_MOVE_COORDINATES = {7, 8, 9, 16};
     private boolean isBackLine;
+    private boolean firsMove;
 
     public Pawn(boolean isWhite, boolean isBackLine) {
         super(isWhite);
         this.isBackLine = isBackLine;
+        firsMove = true;
 
         if (!isWhite){
             if (!isBackLine){
@@ -50,6 +52,73 @@ public class Pawn extends Piece{
     @Override
     public List<Tile2> PossibleMoves(Tile2[][] gameBoard, Tile2 currentTile) {
         List<Tile2> legalMoves = new ArrayList<>();
+
+        int currentX = 0;
+        int currentY = 0;
+
+        //Gets the X and Y coordinates for the tile the user selected
+        for (int ix = 0; ix < gameBoard.length; ix++){
+            for (int iy = 0; iy < gameBoard[ix].length; iy++){
+                if (gameBoard[ix][iy] == currentTile){
+                    currentX = ix;
+                    currentY = iy;
+                }
+            }
+        }
+
+        int moveY = currentY;
+        if ((!isWhite() && !isBackLine || (isWhite() && isBackLine))){
+            if (moveY + 1 == gameBoard[currentX].length){
+                moveY = -1;
+            }
+
+            if (!gameBoard[currentX][moveY + 1].isOccupied()){
+                legalMoves.add(gameBoard[currentX][moveY + 1]);
+            }
+            if (currentX - 1 >= 0){
+                if (gameBoard[currentX - 1][moveY + 1].isOccupied() && gameBoard[currentX - 1][moveY + 1].getPiece().isWhite() != isWhite()){
+                    legalMoves.add(gameBoard[currentX - 1][moveY + 1]);
+                }
+            }
+            if (currentX + 1 <gameBoard.length){
+                if (gameBoard[currentX + 1][moveY + 1].isOccupied() && gameBoard[currentX + 1][moveY + 1].getPiece().isWhite() != isWhite()){
+                    legalMoves.add(gameBoard[currentX + 1][moveY + 1]);
+                }
+            }
+            if (firsMove){
+                if (moveY + 2 == gameBoard[currentX].length){
+                    moveY = -2;
+                }
+                if (!gameBoard[currentX][moveY + 2].isOccupied()){
+                    legalMoves.add(gameBoard[currentX][moveY + 2]);
+                    firsMove = false;
+                }
+            }
+        }else if ((isWhite() && !isBackLine) || (!isWhite() && isBackLine)){
+            if (moveY == 0){
+                moveY = gameBoard[currentX].length;
+            }
+
+            if (!gameBoard[currentX][moveY - 1].isOccupied()){
+                legalMoves.add(gameBoard[currentX][moveY - 1]);
+            }
+            if (currentX - 1 >= 0){
+                if (gameBoard[currentX - 1][moveY - 1].isOccupied() && gameBoard[currentX - 1][moveY - 1].getPiece().isWhite() != isWhite()){
+                    legalMoves.add(gameBoard[currentX - 1][moveY - 1]);
+                }
+            }
+            if (currentX + 1 <gameBoard.length){
+                if (gameBoard[currentX + 1][moveY - 1].isOccupied() && gameBoard[currentX + 1][moveY - 1].getPiece().isWhite() != isWhite()){
+                    legalMoves.add(gameBoard[currentX + 1][moveY - 1]);
+                }
+            }
+            if (firsMove){
+                if (!gameBoard[currentX][moveY - 2].isOccupied()){
+                    legalMoves.add(gameBoard[currentX][moveY - 2]);
+                    firsMove = false;
+                }
+            }
+        }
 
 
         return legalMoves;
