77f7cbb3e4cb91472237581d820dc64846ad7141
Bipin <floriantjager@gmail.com>
2021-07-04 23:43:15 +0200

    mini com

--- Netwerk/src/Pieces/Piece.java
+++ Netwerk/src/Pieces/Piece.java
@@ -10,11 +10,11 @@ import java.util.List;
 
 public abstract class Piece implements Serializable {
 
-//    protected final int piecePosition;
+    //    protected final int piecePosition;
     private Tile2 piecePosition;
     private boolean isWhite;
     private boolean isFirstMove;
-    private transient BufferedImage image;
+    private String imageURL;
     private boolean isKing;
 
     public Piece( boolean isWhite) {
@@ -39,12 +39,12 @@ public abstract class Piece implements Serializable {
 
     public abstract List<Tile2> PossibleMoves(Tile2[][] gameBoard, Tile2 currentTile);
 
-    public BufferedImage getImage() {
-        return image;
+    public BufferedImage getImage() throws IOException {
+        return ImageIO.read(this.getClass().getResourceAsStream(imageURL));
     }
 
-    public void setImage(BufferedImage image) {
-        this.image = image;
+    public void setImage(String image) {
+        this.imageURL = image;
     }
 
     public boolean isKing() {
